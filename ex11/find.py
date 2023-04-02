b42f2f4bd6f0542cd66424ca2f3c71f9f3b48dd9
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-04-02 14:39:56 -0300

    +:I wanna be the very best

---
+++ ex11/find.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+
+poke_name = input("Enter the name of a pokemon: ").strip().lower()
+
+if poke_name == "":
+	print("You did not enter a name.")
+	exit(22)
+try:
+	request = requests.get("https://pokeapi.co/api/v2/pokemon/" + poke_name)
+except requests.HTTPError:
+	print("A HTTP error occurred.")
+	exit()
+if request.status_code == 404:
+	print("Pokemon not found.")
+	exit()
+print("Name: " + request.json()["name"].title())
+print("Abilities:")
+for ability in request.json()["abilities"]:
+	print("-", ability["ability"]["name"].title())
